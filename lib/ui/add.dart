23f6a94aaee9b48c87852dd82b40c7bec2ebeb09
Nanokaaa <khaidirmuktamar234@gmail.com>
2020-11-16 08:32:53 +0700

    nanoka

---
+++ lib/ui/add.dart
@@ -0,0 +1,8 @@
+part of 'uis.dart';
+
+class Add extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/ui/uis.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
+
+part 'homePage.dart';
+part 'add.dart';
